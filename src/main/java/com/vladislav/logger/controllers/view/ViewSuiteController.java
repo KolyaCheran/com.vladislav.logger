720140cbf59d5c8f23adf4f18a9beb17d616cda7
KolyaLoveBublik <nikolay.cheran@gmail.com>
2021-10-06 10:56:08 +0300

    Move reports to the suites view

--- src/main/java/com/vladislav/logger/controllers/view/ViewSuiteController.java
+++ src/main/java/com/vladislav/logger/controllers/view/ViewSuiteController.java
@@ -1,6 +1,8 @@
 package com.vladislav.logger.controllers.view;
 
+import com.vladislav.logger.dao.ReportDAO;
 import com.vladislav.logger.dao.SuiteDAO;
+import com.vladislav.logger.models.Report;
 import com.vladislav.logger.models.Suite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,10 +16,12 @@ import java.util.List;
 public class ViewSuiteController {
 
     private final SuiteDAO suiteDAO;
+    private final ReportDAO reportDAO;
 
     @Autowired
-    public ViewSuiteController(SuiteDAO suiteDAO) {
+    public ViewSuiteController(SuiteDAO suiteDAO, ReportDAO reportDAO) {
         this.suiteDAO = suiteDAO;
+        this.reportDAO = reportDAO;
     }
 
     @GetMapping("/{ids}")
@@ -25,7 +29,17 @@ public class ViewSuiteController {
         List<Suite> suites = suiteDAO.getSuites(ids);
         model.addAttribute("suiteIds", getIdsFromSuites(suites));
         model.addAttribute("suites", suites);
+
+        Report report = reportDAO.getReportBySuiteIDs(getIdsFromSuites(suites));
+        model.addAttribute("report", report);
+        model.addAttribute("passrate", getPassRate(report));
         return "vladislav/suites";
+    }
+
+    private String getPassRate(Report report){
+        double passed = report.getPassedTests().size();
+        double totalFinished = report.getFinishedTests().size();
+        return String.format("%.2f", passed / totalFinished * 100);
     }
 
     private String getIdsFromSuites(List<Suite> suites){
